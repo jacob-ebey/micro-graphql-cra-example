29d4473b3c0a11cf0d9cf98d8bd40f1d08d39bd5
Jacob Ebey <jacob.ebey@live.com>
2020-02-12 22:59:28 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { createCache, createClient, objectHash } from "@micro-graphql/core";
+import { TinyGraphQLProvider, useQuery } from "@micro-graphql/hooks";
+
+const microClient = createClient({
+  fetch,
+  cache: createCache(),
+  hash: objectHash,
+  url: "https://swapi-graphql.netlify.com/.netlify/functions/index"
+});
+
+const Home = () => {
+  const [episodeId, setEpisodeId] = React.useState("ZmlsbXM6MQ==");
+  const handleEpisodeChanged = React.useCallback(
+    event => {
+      event.preventDefault();
+      setEpisodeId(`${event.target.value}`);
+    },
+    [setEpisodeId]
+  );
+
+  const { data, errors, loading } = useQuery(
+    React.useMemo(
+      () => ({
+        query: `
+			query TestQuery($id: ID) {
+				film(id: $id) {
+					id
+					title
+				}
+				allFilms {
+					films {
+						id
+						title
+					}
+				}
+			}
+		`,
+        variables: {
+          id: episodeId
+        }
+      }),
+      [episodeId]
+    )
+  );
+
+  const selector =
+    data && data.allFilms && data.allFilms.films ? (
+      <select defaultValue={`${episodeId}`} onChange={handleEpisodeChanged}>
+        {data.allFilms.films.map(({ title, id }) => (
+          <option key={id} value={`${id}`}>
+            {title || id}
+          </option>
+        ))}
+      </select>
+    ) : null;
+
+  if (loading) {
+    return (
+      <React.Fragment>
+        {selector}
+        <h1>Loading........</h1>
+      </React.Fragment>
+    );
+  }
+
+  if (errors) {
+    return (
+      <React.Fragment>
+        {selector}
+        <h1>Errors...</h1>
+        <pre>
+          <code>{JSON.stringify(errors, null, 2)}</code>
+        </pre>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      {selector}
+      <h1>Data!!!</h1>
+      <pre>
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </React.Fragment>
+  );
+};
+
+const App = () => (
+  <TinyGraphQLProvider client={microClient}>
+    <Home />
+  </TinyGraphQLProvider>
+);
+
+export default App;
